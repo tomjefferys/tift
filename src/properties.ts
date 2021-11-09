4e928ad862d00ae072266f6ed232850717fc53be
Tom Jefferys <tomjefferys@gmail.com>
2021-11-09 21:37:07 +0000

    Refactor parser.  Return a parse result object, which may contain an error

--- src/properties.ts
+++ src/properties.ts
@@ -1,17 +1,161 @@
-const moo = require('moo');
+import * as moo from 'moo';
 
-const lexer = moo.compile({
+const LEXEMES = {
   WS:    /[ \t]+/,
   COLON: /:/,
   NL:    { match: /\n/, lineBreaks: true },
   TEXT:  /[^: \t\n\r]+/,
-});
+};
 
-type PropertyValue = string | number | PropertyMap;
-type PropertyMap = { [key: string]: PropertyValue};
+type HandlerDict = { [key: string]: Function}
+export type PropertyValue = string | number | PropertyMap;
+export type PropertyMap = Map<string,PropertyValue>;
+export type ResultType = PropertyMap | ParseError | undefined;
 
-//const RE_IDENTIFIER = '[a-zA-Z$_][a-zA-Z0-9$_]*';
-//const RE_PROP_START = `^(\\s*)(${RE_IDENTIFIER})\\s*:\\s*(.*?)$`
+export enum ParserStatus {
+  RUNNING,
+  SUCCESS,
+  FAILURE,
+}
+
+export class ParseResult {
+   readonly parserStatus: ParserStatus;
+   readonly result: ResultType;
+
+   constructor(parserStatus: ParserStatus, result: ResultType) {
+     this.parserStatus = parserStatus;
+     this.result = result;
+   }
+
+}
+
+export class ParseError {
+  readonly message: string;
+  readonly linenum: number;
+
+  constructor(message: string, linenum: number) {
+    this.message = message;
+    this.linenum = linenum;
+  }
+}
+
+class Parser {
+  readonly lexer = moo.compile(LEXEMES);
+  readonly handlers : HandlerDict = {
+       "TEXT":  (token: moo.Token) => this.text(token),
+       "COLON": (token: moo.Token) => this.colon(token),
+       "NL":    (token: moo.Token) => this.newLine(token),
+       "WS":    (token: moo.Token) => this.whitespace(token),
+  };
+
+  lineNum: number = 0;
+  propName?: string = undefined;
+  acc: string[] = [];
+  lineAcc: string[] = [];
+  indents: string[] = [""];
+  indent: string = "";
+  maps: PropertyMap[] = [new Map<string,PropertyValue>()];
+  parserStatus = ParserStatus.RUNNING;
+  parseError?: ParseError = undefined;
+
+  nextLine(line: string) {
+    this.lexer.reset(line + "\n");
+    let token = undefined;
+    while(token = this.lexer.next()) {
+      if (token.type) {
+        this.handlers[token.type](token);
+      }
+    }
+  }
+
+  finish() : void {
+    if (this.propName) {
+      this.acc.push(...this.lineAcc);
+      peek(this.maps).set(this.propName, this.acc.join(" "));
+    }
+    this.parserStatus = ParserStatus.SUCCESS;
+  }
+
+  getResult() : ParseResult {
+    switch(this.parserStatus) {
+      case ParserStatus.RUNNING:
+        return new ParseResult(ParserStatus.RUNNING, undefined);
+      case ParserStatus.FAILURE:
+        return new ParseResult(ParserStatus.FAILURE, this.parseError);
+      case ParserStatus.SUCCESS:
+        return new ParseResult(ParserStatus.SUCCESS, this.maps[0]);
+    }
+  }
+  
+  text(token: moo.Token) {
+    this.lineAcc.push(token.value);
+  }
+
+  // handle a colon, this may mean a new 
+  // property definition
+  colon(token: moo.Token) {
+    if (this.lineAcc.length == 1) {
+      // Check the indent, if it's greater than previous 
+      // then this is a child property
+      if (this.indent.length > peek(this.indents).length) {
+        this.indents.push(this.indent);
+        let newMap = new Map();
+        // FIXME propName could be undefined
+        peek(this.maps).set(this.propName!, newMap);
+        this.maps.push(newMap);
+
+      } else if (this.indent === peek(this.indents)) {
+        // If same then sibling
+        // don't need to do anything
+        if (this.propName) {
+          this.setProperty();
+        }
+      } else {
+        // set the prop
+        this.setProperty();
+
+        // pop the indent need to find match
+        while(this.indents.pop()) {
+          this.maps.pop();
+          if(this.indent === peek(this.indents)) {
+            break;
+          }
+        }
+        //maps.pop();
+      }
+      
+      this.propName = this.lineAcc[0];
+      this.lineAcc.length = 0;
+    } else {
+      this.lineAcc.push(token.value);
+    }
+  }
+
+  // handle a newLine
+  // Resets the line accumultor and indent
+  newLine(token: moo.Token) {
+    this.acc.push(...this.lineAcc);
+    this.indent = "";
+    this.lineAcc = [];
+  }
+
+  // handle whitespace
+  // If it's the first on a line record it as an indent
+  whitespace(token: moo.Token) {
+    if (!this.indent && this.lineAcc.length == 0) {
+      this.indent = token.value;
+    }
+  }
+
+
+  // Set the currently parsing property with the value of the
+  // accumulator
+  setProperty() : void {
+      peek(this.maps).set(this.propName!, this.acc.join(" "));
+      this.acc.length = 0;
+  }
+
+}
 
 
 // Tokenize a properti string, adding INDENT DEDENT symbols 
@@ -25,82 +169,11 @@ function peek<Type>(list: Type[]) : Type {
   return list[list.length - 1];
 }
 
-export function parseProperties(lines: string[]) {
-  let propName = undefined;
-  let acc = [];
-  let lineAcc = [];
-  let indents = [""];
-  let indent = "";
-  let maps = [new Map()];
+export function parseProperties(lines: string[]) : ParseResult {
+  const parser = new Parser();
   for(const line of lines) {
-    lexer.reset(line + "\n");
-    
-    let lexeme = undefined;
-    while(lexeme = lexer.next()) {
-        switch(lexeme.type) {
-          case "TEXT":
-            lineAcc.push(lexeme.value);
-            break;
-          case "COLON":
-            if (lineAcc.length == 1) {
-              // Check the indent, if it's greater than previous 
-              // then this is a child property
-              // TODO check that all other indents are 
-              //      prefixes of this indent
-              if (indent.length > peek(indents).length) {
-                indents.push(indent);
-                let newMap = new Map();
-                peek(maps).set(propName, newMap);
-                maps.push(newMap);
-
-              } else if (indent === peek(indents)) {
-                // If same then sibling
-                // don't need to do anything
-                if (propName) {
-                  peek(maps).set(propName, acc.join(" "));
-                  acc.length = 0;
-                }
-              } else {
-                // set the prop
-                peek(maps).set(propName, acc.join(" "));
-                acc.length = 0;
-
-                // pop the indent need to find match
-                while(indents.pop()) {
-                  maps.pop();
-                  if(indent === peek(indents)) {
-                    break;
-                  }
-                }
-                //maps.pop();
-              }
-              
-              propName = lineAcc[0];
-              lineAcc.length = 0;
-            } else {
-              lineAcc.push(lexeme.value);
-            }
-            break;
-          case "NL":
-            acc.push(...lineAcc);
-            indent = "";
-            lineAcc = [];
-            break;
-          case "WS":
-            if (!indent && lineAcc.length == 0) {
-              indent = lexeme.value;
-            }
-            break;
-          default:
-            break;
-        }
-    }
+    parser.nextLine(line);
+    parser.finish();
   }
-  if (propName) {
-    acc.push(...lineAcc);
-    peek(maps).set(propName, acc.join(" "));
-  }
-  
-
-  return maps[0];
+  return parser.getResult();
 }
