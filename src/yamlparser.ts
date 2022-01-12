25290bf9561fd8b836f70c79da77022701272326
Tom Jefferys <tomjefferys@gmail.com>
2022-01-12 21:43:39 +0000

    Add basic yaml parser

---
+++ src/yamlparser.ts
@@ -0,0 +1,42 @@
+import { loadAll } from "js-yaml"
+import * as fs from "fs"
+
+const data = fs.readFileSync("test.yaml", "utf8");
+
+let prototypes = {
+  "room":{"type":"room"},
+  "object":{"type":"object"},
+  "rule":{"type":"rule"}, 
+}
+
+export type ObjValue = Obj | ObjArray | string | number;
+export type ObjArray = ObjValue[];
+export type Obj = {[key:string] : ObjValue};
+
+function loadObjs(fileName: string) {
+  const data = fs.readFileSync(fileName, "utf8");
+  return getObjs(data);
+}
+
+function getObjs(data: string) {
+  let docs : Obj[] = [];
+  loadAll(data, rawDoc => { 
+    const doc = rawDoc as Obj
+    for(const [name, pt] of Object.entries(prototypes)) {
+      if (doc[name]) {
+        let newDoc : Obj = Object.assign({"name": doc[name]}, pt, doc);
+        delete newDoc[name];
+        docs.push(newDoc);
+      }
+    } 
+  });
+  return docs;
+}
+
+const docs = loadObjs("test.yaml");
+
+for(const doc of docs) {
+  console.log(JSON.stringify(doc, null, 2));
+
+}
+
