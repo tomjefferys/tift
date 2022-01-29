78cbf47645a26818beb108b23896d3645ece7341
Tom Jefferys <tomjefferys@gmail.com>
2022-01-29 21:38:09 +0000

    Set up node package management, tidy up ts, and jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   moduleFileExtensions: ['js'],
   testMatch: ["**/out/test/**/*.js"],
