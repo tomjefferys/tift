61623e51fcea5d5f2e0e458fe65d70599669c178
Tom Jefferys <tomjefferys@gmail.com>
2022-06-02 11:01:09 +0100

    Add action matcher

---
+++ engine/src/actionmatcher.ts
@@ -0,0 +1,65 @@
+import { Verb } from "./verb";
+
+type BindingMap = {[key:string]:string};
+
+// A match result is true/false + bindings
+export interface MatchResultSuccess {
+    readonly match : true,
+    bindings : BindingMap
+}
+
+export interface MatchResultFailure {
+    readonly match : false
+}
+
+export type MatchResult = MatchResultSuccess | MatchResultFailure;
+
+export type ActionMatcher = (words: string[]) => MatchResult;
+
+interface WordMatch {
+    isCapture : false;
+    match : string;
+}
+
+interface Capture {
+    isCapture : true;
+    name : string;
+}
+
+type Match = WordMatch | Capture;
+
+export function match(word : string) : Match {
+    return {
+        isCapture : false,
+        match : word
+    }
+}
+
+export function capture(name : string) : Match {
+    return {
+        isCapture : true,
+        name : name
+    }
+}
+
+export function getMatcher(expected : Match[]) : ActionMatcher {
+    return (words: string[]) => {
+        const fail : MatchResult = { match : false };
+        if (words.length != expected.length) {
+            return fail;
+        }
+
+        const bindings : BindingMap = {};
+        let matchSuccess = true;
+        for(let i = 0; i<words.length; i++) {
+            const matcher = expected[i];
+            if (matcher.isCapture) {
+                bindings[matcher.name] = words[i];
+            } else if (matcher.match !== words[i]) {
+                matchSuccess = false;
+                break;
+            }
+        }
+        return (matchSuccess) ?  { match : true, bindings : bindings } : fail;
+    } 
+}
---
+++ engine/test/actionmatchertest.ts
@@ -0,0 +1,34 @@
+import { this_str } from "jsep";
+import { match, capture, getMatcher, MatchResultSuccess } from "../src/actionmatcher"
+
+test("test empty input", () => {
+    const matcher = getMatcher([match("one"), match("two"), match("three")]);
+    const result = matcher([""]);
+    expect(result.match).toBe(false);
+})
+
+test("test empty match", () => {
+    const matcher = getMatcher([]);
+    let result = matcher(["one", "two", "three"]);
+    expect(result.match).toBe(false);
+
+    result = matcher([]);
+    expect(result.match).toBe(true);
+    expect((result as MatchResultSuccess).bindings).toStrictEqual({}):
+})
+
+test("test simple match", () => {
+    const matcher = getMatcher([match("one"), match("two"), match("three")]);
+    const result = matcher(["one", "two", "three"]);
+    expect(result.match).toBe(true);
+    const success = result as MatchResultSuccess;
+    expect(success.bindings).toStrictEqual({});
+})
+
+test("test simple capture", () => {
+    const matcher = getMatcher([match("one"), capture("number"), match("three")]);
+    const result = matcher(["one", "two", "three"]);
+    expect(result.match).toBe(true);
+    const success = result as MatchResultSuccess;
+    expect(success.bindings).toStrictEqual({"number": "two"});
+})
