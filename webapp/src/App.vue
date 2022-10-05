27bf8806153d739281c2964d290d60853381b72a
Tom Jefferys <tomjefferys@gmail.com>
2022-10-05 20:04:00 +0100

    Webapp loads game from separate file

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -11,42 +11,15 @@ import { OutputConsumer, OutputMessage } from '@engine/messages/output.ts'
 //@ts-ignore
 import { IdValue } from '@engine/shared.ts'
 
-const adventure = `
----
-room: cave
-desc: A dark dank cave
-exits:
-  north: entrance
-  south: pool
-tags: [start]
----
-room: entrance
-desc: Sunlight casts a pool of illumination over the rocky and uneven floor
-exits:
-  south: cave
----
-room: pool
-desc: A deep pool of cold clear water exends over the southern end of the chamber
-exits:
-  north: cave
----
-item: key
-name: rusty key
-desc: An old rusty key
-location: pool
-tags: [carryable]
----
-item: hotRock
-name: hot rock
-desc: a burning hot piece of recently solidified lava
-location: entrance
-tags: [carryable]
-before: get(hotRock) => "Ouch!"
----
-rule: rule1
-run:
-  - if(random(1,2) == 1).then(print("A cold wind runs straight through you"))
-`;
+fetch('./adventure.yaml')
+  .then((response) => response.text())
+  .then(data => {
+    load(data);
+    start();
+    getStatus();
+    getWords([]);
+    console.log("Loaded!");
+  })
 
 const output : OutputMessage[] = [];
 const engine : Engine = getEngine((message: OutputMessage) => output.push(message));
@@ -57,12 +30,6 @@ const state = reactive({
   text : [] as string[],
   status : ""
   });
-
-
-load(adventure);
-start();
-getStatus();
-getWords([]);
 
 function wordSelected(word: IdValue) {
   state.command.push(word.id);
