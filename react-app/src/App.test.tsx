cce37b52eedeb041a849777e701749969bba9888
Tom Jefferys <tomjefferys@gmail.com>
2022-12-28 15:52:14 +0000

    Add basic smoke test for the web app

--- react-app/src/App.test.tsx
+++ react-app/src/App.test.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import { rest } from 'msw'
+import {setupServer} from 'msw/node'
 import App from './App';
 
-test('renders learn react link', () => {
+const TEST_DATA = `
+---
+room: cave
+desc: A dark dank cave
+tags: [start]
+`
+
+const server = setupServer(
+  rest.get('/adventure.yaml', (req, res, ctx) => {
+    return res(ctx.body(TEST_DATA));
+  })
+)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+test('renders learn react link', async () => {
+  window.HTMLElement.prototype.scrollIntoView = function() {};
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  await waitFor(() => screen.getAllByText('cave'));
+  const linkElement = screen.getAllByText('cave');
+  expect(linkElement[0]).toBeInTheDocument();
 });
